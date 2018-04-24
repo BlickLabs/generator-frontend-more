9e49d5098312e21df8bd182024dc63d5d64d8be3
Noe Villegas <noe.villegas333@gmail.com>
2018-04-24 13:15:15 -0500

    Add image optimizer

--- generators/app/templates/gulptasks/images.js
+++ generators/app/templates/gulptasks/images.js
@@ -1,13 +1,13 @@
 var gulp = require('gulp'),
   paths = require('../gulpconfig').paths,
-  imagemin = require('gulp-image'),
+  image = require('gulp-image'),
   gulpIf = require('gulp-if'),
   argv = require('yargs').argv,
   production = argv.production;
 
 gulp.task('copy:images', function () {
   gulp.src(paths.getSrc('img'))
-    .pipe(gulpIf(production, imagemin()))
+    .pipe(gulpIf(production, image()))
     .pipe(gulp.dest(paths.getCompiled(production, 'img')));
 
   gulp.src(paths.getSrc('favicon'))
